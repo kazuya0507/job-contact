03a866e75b35d8fb3e0906a1e5aa1f1297ab760d
kazuya0507 <fas0507@outlook.jp>
2020-08-30 00:13:02 +0900

    fix-3

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,11 +12,11 @@ set :repo_url,  'git@github.com:kazuya0507/job-contact.git'
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
-set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2.3.1です
+set :rbenv_ruby, '2.5.1'
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/contact.pem'] 
+                  keys: ['['~/.ssh/job-contact.pem'] 
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
@@ -26,37 +26,9 @@ set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
 # デプロイ処理が終わった後、Unicornを再起動するための記述
-set :linked_files, %w{config/master.key}
-
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
-
- task :restart do
-   invoke 'unicorn:restart'
- end
-# restartだとキャッシュが残るので下記の書き方でも良い
-# task :restart do
-#   invoke 'unicorn:stop'
-#   invoke 'unicorn:start'
-# end
-
- desc 'upload master.key'
- task :upload do
-   on roles(:app) do |host|
-     if test "[ ! -d #{shared_path}/config ]"
-       execute "mkdir -p #{shared_path}/config"
-     end
-     upload!('config/master.key', "#{shared_path}/config/master.key")
-   end
- end
- before :starting, 'deploy:upload'
- after :finishing, 'deploy:cleanup'
+  task :restart do
+    invoke 'unicorn:restart'
+  end
 end
-
-# 必要に応じて/環境変数をcapistranoでの自動デプロイで利用
-set :default_env, {
-  rbenv_root: "/usr/local/rbenv",
-  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
-  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
-  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
-}
