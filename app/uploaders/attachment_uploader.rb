feee32ec8a88551a4ff9ee868f781b720a6ea40c
kazuya0507 <fas0507@outlook.jp>
2020-09-10 12:51:16 +0900

    fix

--- app/uploaders/attachment_uploader.rb
+++ app/uploaders/attachment_uploader.rb
@@ -1,6 +1,6 @@
 class AttachmentUploader < CarrierWave::Uploader::Base
-  storage :file
-  # storage :fog
+  # storage :file
+  storage :fog
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
