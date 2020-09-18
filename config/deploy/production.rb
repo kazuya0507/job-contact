307b7d9fcdec2165d8f4b0c823f8480928cba9c9
kazuya0507 <fas0507@outlook.jp>
2020-09-18 22:39:25 +0900

    ip fix

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server '54.178.107.158', user: 'ec2-user', roles: %w{app db web}
+server '176.34.5.73', user: 'ec2-user', roles: %w{app db web}
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
